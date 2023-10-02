4aac7a6c85e966e99e558764536151c0d69558b7
Stefano Romani <fanoromani@gmail.com>
2023-10-02 15:32:52 -0300

    uncommented api route

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 export const api = axios.create({
-  baseURL: /* import.meta.env.VITE_BACKEND_URL */ "http://localhost:10000",
+  baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:10000",
 });
 
 api.interceptors.request.use(
